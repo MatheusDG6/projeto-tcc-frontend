089b1e4839b98737a6c470b962af3ab7a370720a
Matheus <matheusdg.2006@gmail.com>
2026-06-25 10:20:00 -0300

    iniciando projeto frontend

---
+++ src/main/java/com/projeto/tcc_frontend/controller/UsuarioController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.projeto.tcc_frontend.controller;
+
+import com.projeto.tcc_frontend.model.UsuarioBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ *
+ * @author mathe
+ */
+@Controller
+public class UsuarioController {
+/* 
+    @PostMapping("/cadastrar")
+    public String mandarRegistro(@ModelAttribute UsuarioBean usuario ) {
+        restService.registrar(usuario);
+        return "cadastrar";
+    }
+*/
+}
---
+++ src/test/java/com/projeto/tcc_frontend/TccFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.tcc_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
